7e96539b86ffff743f5b74c8aecba2d18c7ea855
LennyFaceCoin <arii.sichoix@gmail.com>
2022-05-24 13:54:20 -0400

    upload

--- src/ipfs-meta/test.js
+++ src/ipfs-meta/test.js
@@ -1,7 +1,7 @@
 var { getFilesFromPath, Web3Storage } =require('web3.storage')
 var fs = require('fs');
 var axios = require('axios')
-var data = require('../extra/json/cid_info.json')
+var data = require('../../extra/json/cid_info.json')
 
 const sleep = ms => {
     return new Promise(resolve => setTimeout(resolve, ms))
